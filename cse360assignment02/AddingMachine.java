d918526675c4a0f52d0330fb3653eb972f6c6992
Sandb <Sandb@DESKTOP-U8MFTUS>
2020-10-05 13:55:35 -0700

    first checkin of AddingMachine

---
+++ cse360assignment02/AddingMachine.java
@@ -0,0 +1,44 @@
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+  
+  public AddingMachine () {
+	  /**
+	   * this method is the constructor, and initializes total
+	   */
+    total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () {
+	  /**
+	   * This method gets the total
+	   */
+    return 0;
+  }
+  
+  public void add (int value) {
+	  /** 
+	   * This method adds to the total
+	   */
+  }
+
+  public void subtract (int value) {
+	  /** 
+	   * this method subtracts from the total
+	   */
+  }
+
+  public String toString () {
+	  /**
+	   * this method will return the total as a String
+	   */
+    return "";
+  }
+
+  public void clear() {
+	  /**
+	   * This method will clear the total
+	   */
+  }
+}
