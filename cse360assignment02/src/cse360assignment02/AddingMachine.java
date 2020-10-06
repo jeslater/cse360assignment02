f80d05d694e207d43b0126853e99322dc3356092
Sandb <Sandb@DESKTOP-U8MFTUS>
2020-10-05 17:42:56 -0700

    Created the files as classes so it would run in Eclipse

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,52 @@
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+  private String str = "0";
+  
+  public AddingMachine () {
+	  /**
+	   * this method is the constructor, and initializes total
+	   */
+    total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () {
+	  /**
+	   * This method gets the total
+	   */
+    return total;
+  }
+  
+  public void add (int value) {
+	  /** 
+	   * This method adds to the total
+	   */
+	  str = str + " + " + Integer.toString(value);
+	  total = total + value;
+  }
+
+  public void subtract (int value) {
+	  /** 
+	   * this method subtracts from the total
+	   */
+	  str = str + " - " + Integer.toString(value);
+	  total = total - value;
+  }
+
+  public String toString () {
+	  /**
+	   * this method will return the total as a String
+	   */
+			  
+    return str;
+  }
+
+  public void clear() {
+	  /**
+	   * This method will clear memory
+	   */
+	  total = 0;
+	  str = "";
+  }
+}
